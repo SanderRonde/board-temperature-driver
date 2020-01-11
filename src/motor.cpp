db82164f4521f22b1544756c7e8008187e4ac810
Sander Ronde <awsdfgvhbjn@gmail.com>
2020-01-11 18:42:55 +0100

    add/change logs

--- src/motor.cpp
+++ src/motor.cpp
@@ -1,20 +1,22 @@
 #include <Arduino.h>
+#include <telnet.h>
 
-#define MOTOR_ON_PIN 6
-#define MOTOR_RIGHT_PIN 7
+#define MOTOR_ON_PIN D6
+#define MOTOR_RIGHT_PIN D7
 
 // TODO: change/measure
 #define FULL_MOVE_SECS 20
-#define MAX_PROG 1000
-#define PROG_PER_SEC MAX_PROG / FULL_MOVE_SECS
 
 namespace Motor {
-	int progress = 0;
-
 	typedef enum DIR {
 		LEFT,
 		RIGHT
 	} dir_t;
+
+	void setup() {
+		pinMode(MOTOR_ON_PIN, OUTPUT);
+		pinMode(MOTOR_RIGHT_PIN, OUTPUT);
+	}
 
 	void start_motor(dir_t dir) {
 		if (dir == LEFT) {
@@ -32,20 +34,15 @@ namespace Motor {
 	}
 
 	void move_left() {
+		LOGN("Moving left");
 		start_motor(LEFT);
-		delay((progress * 1000) / PROG_PER_SEC);
-		progress = 0;
+		delay(FULL_MOVE_SECS * 1000);
 		stop_motor();
 	}
 	void move_right() {
+		LOGN("Moving right");
 		start_motor(RIGHT);
-		delay(((MAX_PROG - progress) * 1000) / PROG_PER_SEC);
-		progress = MAX_PROG;
+		delay(FULL_MOVE_SECS * 1000);
 		stop_motor();
-	}
-
-	void setup() {
-		pinMode(MOTOR_ON_PIN, OUTPUT);
-		pinMode(MOTOR_RIGHT_PIN, OUTPUT);
 	}
 }
