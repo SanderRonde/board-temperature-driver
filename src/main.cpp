62c6cc95bb3e8cfd7b4391b4b972477ae6c7222a
Sander Ronde <awsdfgvhbjn@gmail.com>
2020-01-11 18:44:18 +0100

    add some logging and use new libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,34 @@
+#include <ArduinoOTA.h>
 #include <Arduino.h>
+#include <telnet.h>
+#include <ota.h>
 #include <motor.h>
 #include <temp.h>
 #include <net.h>
 
 void setup() {
 	Serial.begin(115200);
+	Serial.println("Booting");
 
+	// Setup telnet
+	Telnet::setup("temp-board");
+
+	// Setup OTA and wait
+	OTA::setup();
+	LOGN("Booted. Waiting for possible OTAs");
+	OTA::wait_for_otas();
+	LOGN("Stopped waiting");
+
+	// Setup the rest
 	Motor::setup();
 	Temp::setup();
 	Net::setup();
+
+	// Done
+	LOGN("Booted");
 }
 
 void loop() {
+	OTA::loop();
 	Temp::loop();
 }
